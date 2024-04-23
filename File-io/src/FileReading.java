837bfebc53d9a1967ab9fee6e0b5d9df497a7d57
Achint <achintk89@gmail.com>
2024-04-23 10:33:49 +0530

    New push

---
+++ File-io/src/FileReading.java
@@ -0,0 +1,34 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Scanner;
+
+public class FileReading {
+    public static void main(String[]args) throws IOException {
+        FileWriter myWriter = new FileWriter("filename.txt");
+        Scanner s = new Scanner(System.in);
+        String str1,response;
+
+        while(true)
+        {
+            System.out.println("Enter text to be written -");
+            str1 = s.nextLine();
+            myWriter.write(str1);
+            System.out.println("Want to add more?(y/n)");
+            response = s.nextLine();
+            if(response.equals("y"))
+                continue;
+            else {
+                myWriter.close();
+                break;
+            }
+        }
+        FileReader fr = new FileReader("filename.txt");
+        int i;
+
+        while ((i = fr.read()) != -1)
+            System.out.print((char)i);
+    }
+
+}
--- Wiley Edge/src/HelloWorld.java
+++ Wiley Edge/src/HelloWorld.java
@@ -1,9 +1,20 @@
-public class HelloWorld implements A{
-    public String getName(){
-        return "name";
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+
+    {
+        printNums(12);
     }
-    public static void main (String[] args){
-        HelloWorld t = new HelloWorld();
-        System.out.println(t.getName());
+
+    public static void printNums(int number)
+    {
+        String s1="welcome";
+        String s2=new String("welcome");
+        if(s1==s2)
+            System.out.println("same");
+        else
+            System.out.println("not same");
     }
+
 }
